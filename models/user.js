2202fc138ca56fbdd4eb7291a5cc4f973a233ec0
David Edgar <david.r.edgar@gmail.com>
2017-05-02 19:59:30 +0100

    no db for now

--- models/user.js
+++ models/user.js
@@ -7,14 +7,14 @@ var url = 'mongodb://localhost:27017/myproject';
 
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
+/*MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
   insertDocuments(db, function() {
     db.close();
   });
-});
+});*/
 
 
 var insertDocuments = function(db, callback) {
